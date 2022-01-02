ee55dcffe5844525fb1c8af72c36cf19ed1089da
whaberer <91747264+willhaberer@users.noreply.github.com>
2022-01-02 10:31:24 -0500

    updated about me page with an image

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,12 +4,21 @@ import "../../styles/style.css";
 export default function About() {
   return (
     <div class="main-content">
-      <h1>About Page</h1>
-      <p>
-        Will Haberer is a web developement student at Columbia Coding Bootcamp
-        learning full-stack development through HTML, CSS and Javascript. He is
-        based in Arlington Virginia.
-      </p>
+      <h1>About Me</h1>
+
+      <div class="row about-me">
+        <div class="col-6">
+          Will Haberer is a web developement student at Columbia Coding Bootcamp
+          learning full-stack development through HTML, CSS and Javascript. He
+          is based in Arlington Virginia.
+        </div>
+        <div class="col-3">
+          <img
+            src="https://www.trilogyed.com/blog/wp-content/uploads/2018/05/columbia_coding_boot_camp2_brandon_colbert.jpg"
+            alt="Columbia Coding Bootcamps"
+          ></img>
+        </div>
+      </div>
     </div>
   );
 }
