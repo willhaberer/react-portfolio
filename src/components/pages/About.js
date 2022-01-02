8048d75acd6839627cbb44ebe5d696c94a4e4ace
whaberer <91747264+willhaberer@users.noreply.github.com>
2022-01-02 10:48:29 -0500

    added more sections to the about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,9 +4,8 @@ import "../../styles/style.css";
 export default function About() {
   return (
     <div class="main-content">
-      <h1>About Me</h1>
-
-      <div class="row about-me">
+      <h1 id="page-header">About Me</h1>
+      <div class="row me-info">
         <div class="col-6">
           Will Haberer is a web developement student at Columbia Coding Bootcamp
           learning full-stack development through HTML, CSS and Javascript. He
@@ -16,6 +15,32 @@ export default function About() {
           <img
             src="https://www.trilogyed.com/blog/wp-content/uploads/2018/05/columbia_coding_boot_camp2_brandon_colbert.jpg"
             alt="Columbia Coding Bootcamps"
+            id="about-image"
+          ></img>
+        </div>
+      </div>
+      <div class="row work-info">
+        <div class="col-3">
+          <img
+            src="https://www.trilogyed.com/blog/wp-content/uploads/2018/05/columbia_coding_boot_camp2_brandon_colbert.jpg"
+            alt="Columbia Coding Bootcamps"
+            id="about-image"
+          ></img>
+        </div>
+        <div class="col-6" id="work-text">
+          My previous work is certainly something
+        </div>
+      </div>
+      <div class="row hobby-info">
+        <div class="col-6">
+          In my free time i enjoy cooking, hiking and working on creative
+          projects such as making videos, coding projects and creating puzzles
+        </div>
+        <div class="col-3">
+          <img
+            src="https://www.trilogyed.com/blog/wp-content/uploads/2018/05/columbia_coding_boot_camp2_brandon_colbert.jpg"
+            alt="Columbia Coding Bootcamps"
+            id="about-image"
           ></img>
         </div>
       </div>
