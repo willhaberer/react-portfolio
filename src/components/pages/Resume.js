67606ad591aaa027c794ba2759711dc835a9cf51
whaberer <91747264+willhaberer@users.noreply.github.com>
2022-01-03 18:18:36 -0500

    updated resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,24 +1,33 @@
 import React from "react";
 import "../../styles/style.css";
-import wh from "../../assets/coding-info.png";
 
 export default function Home() {
   return (
-    <div class="main-content">
-      <h1>Home Page</h1>
-      <img src={wh} alt="Coding" id="coding-image"></img>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
-    </div>
+    <>
+      <div class="resumeContainer">
+        <h1>Work Experience</h1>
+        <ul id="resumeWorkList">
+          <li>Digital and Data Director-WRS, August 2020-June 2021</li>
+          <li>Administrative Assistant-Benefits in A Card, May-July 2019</li>
+        </ul>
+      </div>
+      <div class="resumeContainer">
+        <h1>Volunteer Experience</h1>
+        <ul id="resumeVolList">
+          <li>
+            Head Coach-Soccer Organization of the Charlottesville Area,
+            September 2017- October 2020
+          </li>
+          <li>Head Coach-Nashville Sled Preds Tier V Adult, 2015-2017</li>
+        </ul>
+      </div>
+      <div class="resumeContainer">
+        <h1>Education</h1>
+        <ul id="resumeVolList">
+          <li>Columbia University Web Development Bootcamp- 2021</li>
+          <li>University of Virginia BA, Government- 20127-2020</li>
+        </ul>
+      </div>
+    </>
   );
 }
