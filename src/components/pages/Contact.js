bbeb46831e2b80baebbb48ca5bf36b75be28bcf8
whaberer <91747264+willhaberer@users.noreply.github.com>
2022-01-03 17:54:55 -0500

    finished contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,33 +2,55 @@ import React from "react";
 import "../../styles/style.css";
 
 export default function Contact() {
-  const styles = {
-    forms: {
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      gap: "7px",
-    },
+  const handleSubmit = (event) => {
+    event.preventDefault();
   };
+
   return (
-    <div class="main-content">
-      <h1>Contact Page</h1>
-      <div style={styles.forms} class="window-body">
-        <div class="field-row">
-          <label for="text17">Name:</label>
-          <input id="text17" type="text" />
+    <div class="contact-content">
+      <div className="jumbotron jumbotron-fluid">
+        <div className="contactContainer">
+          <br></br>
+          <h2>
+            Best Way to Reach Me? Send Me an Email:{" "}
+            <a href="mailto:vawhab@gmail.com">Here</a>
+          </h2>
+          <br></br>
+          <h3>Otherwise please enter your information here</h3>
+          <form className="contact-form">
+            <div className="contact-input">
+              <label className="label">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              ></input>
+            </div>
+            <div className="contact-input">
+              <label className="label">Name</label>
+              <input type="text" className="form-control" id="name"></input>
+            </div>
+            <div className="form-group white">
+              <label className="label">Leave me a brief message!</label>
+              <textarea
+                className="form-control"
+                id="message"
+                rows="3"
+                // ref={messageRef}
+              ></textarea>
+            </div>
+            <br></br>
+            <button
+              type="submit"
+              id="submitBtn"
+              className="btn btn-primary button label"
+              onSubmit={() => handleSubmit()}
+            >
+              Submit
+            </button>
+          </form>
         </div>
-        <div class="field-row">
-          <label for="text17">Email:&nbsp; </label>
-          <input id="text17" type="text" />
-        </div>
-        <div class="field-row">
-          <label for="text17">Message:&nbsp; &nbsp;</label>
-          <input id="text17" type="text" />
-        </div>
-        <form>
-          <button type="submit">Submit</button>
-        </form>
       </div>
     </div>
   );
