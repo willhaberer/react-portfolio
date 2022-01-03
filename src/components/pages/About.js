7617dcb1cbc711cbdeeab030c5d659aa09014a67
whaberer <91747264+willhaberer@users.noreply.github.com>
2022-01-03 16:36:46 -0500

    finished about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,50 +1,19 @@
 import React from "react";
 import "../../styles/style.css";
+import coverPhoto from "../../assets/coding-info.png";
 
 export default function About() {
   return (
     <div class="main-content">
-      <h1 id="page-header">About Me</h1>
-      <div class="row me-info">
-        <div class="col-6">
-          Will Haberer is a web developement student at Columbia Coding Bootcamp
-          learning full-stack development through HTML, CSS and Javascript. He
-          is based in Arlington Virginia.
-        </div>
-        <div class="col-3">
-          <img
-            src="https://www.trilogyed.com/blog/wp-content/uploads/2018/05/columbia_coding_boot_camp2_brandon_colbert.jpg"
-            alt="Columbia Coding Bootcamps"
-            id="about-image"
-          ></img>
-        </div>
-      </div>
-      <div class="row work-info">
-        <div class="col-3">
-          <img
-            src="https://www.trilogyed.com/blog/wp-content/uploads/2018/05/columbia_coding_boot_camp2_brandon_colbert.jpg"
-            alt="Work"
-            id="about-image"
-          ></img>
-        </div>
-        <div class="col-6" id="work-text-container">
-          <p id="work-text">My previous work is certainly something</p>
-        </div>
-      </div>
-      <div class="row hobby-info">
-        <div class="col-6">
-          <p>
-            In my free time i enjoy cooking, hiking and working on creative
-            projects such as making videos, coding projects and creating puzzles
-          </p>
-        </div>
-        <div class="col-3">
-          <img
-            src="https://cdn.pixabay.com/photo/2016/02/05/15/34/pasta-1181189_1280.jpg"
-            alt="Cooking"
-            id="about-image"
-          ></img>
-        </div>
+      <div class="aboutCard">
+        <h1 id="page-header">About Me</h1>
+        <img src={coverPhoto} alt="CoverPhoto" className="cover-photo"></img>
+        <br></br>
+        <p>
+          Hello! I'm a web developement student at Columbia Coding Bootcamp
+          learning full-stack development through HTML, CSS and Javascript.
+          Currently I am based in Arlington Virginia.
+        </p>
       </div>
     </div>
   );
