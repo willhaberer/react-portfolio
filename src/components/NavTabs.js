275e417eb87ac5720b87c6d7a08cd26982da5778
Will Haberer <willhaberer@gmail.com>
2022-01-10 10:55:30 -0500

    reworked navtabs to incorporate a button toggle

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -3,52 +3,75 @@ import "../styles/style.css";
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <nav id="navbar">
-      <h2 id="nav-name">Will Haberer</h2>
-      <ul className="nav nav-tabs">
-        <li className="nav-item" id="nav-item">
-          <a
-            href="#about"
-            onClick={() => handlePageChange("About")}
-            className={currentPage === "About" ? "nav-link active" : "nav-link"}
-          >
-            About
-          </a>
-        </li>
-        <li className="nav-item" id="nav-item">
-          <a
-            href="#portfolio"
-            onClick={() => handlePageChange("Portfolio")}
-            className={
-              currentPage === "Portfolio" ? "nav-link active" : "nav-link"
-            }
-          >
-            Portfolio
-          </a>
-        </li>
-        <li className="nav-item" id="nav-item">
-          <a
-            href="#home"
-            onClick={() => handlePageChange("Resume")}
-            className={
-              currentPage === "Resume" ? "nav-link active" : "nav-link"
-            }
-          >
-            Resume
-          </a>
-        </li>
-        <li className="nav-item" id="nav-items">
-          <a
-            href="#contact"
-            onClick={() => handlePageChange("Contact")}
-            className={
-              currentPage === "Contact" ? "nav-link active" : "nav-link"
-            }
-          >
-            Contact
-          </a>
-        </li>
-      </ul>
+    <nav
+      id="navbar"
+      className="navbar navbar-expand-lg navbar-light navigation blue"
+    >
+      <h2 className="navbar-brand bitter" id="head">
+        Will Haberer
+      </h2>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarTogglerDemo02"
+        aria-controls="navbarTogglerDemo02"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse blue" id="navbarTogglerDemo02">
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li className="nav-item">
+            <a
+              className={
+                currentPage === "About"
+                  ? "nav-link active minor"
+                  : "nav-link minor"
+              }
+              href="#about"
+              onClick={() => handlePageChange("About")}
+            >
+              About Me
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className={
+                currentPage === "Portfolio" ? "nav-link active" : "nav-link"
+              }
+              href="#portfolio"
+              onClick={() => handlePageChange("Portfolio")}
+            >
+              Portfolio
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className={
+                currentPage === "Contact" ? "nav-link active" : "nav-link"
+              }
+              href="#contact"
+              onClick={() => handlePageChange("Contact")}
+            >
+              Contact Me
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className={
+                currentPage === "Resume" ? "nav-link active" : "nav-link"
+              }
+              href="#resume"
+              onClick={() => handlePageChange("Resume")}
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
