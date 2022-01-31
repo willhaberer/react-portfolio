0c98d6c5858d73bf913fd0e61aa424654adc91f5
Will Haberer <willhaberer@gmail.com>
2022-01-31 12:56:15 -0500

    added pictures to portfolios

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "../../styles/style.css";
+import FMSN from "../../assets/FMSN.png";
+import Ftnss from "../../assets/Ftnss.png";
+import WD from "../../assets/WD.png";
+import PG from "../../assets/PG.png";
 
 export default function Portfolio() {
   return (
@@ -13,6 +17,12 @@ export default function Portfolio() {
               <div class="card">
                 <div class="card-body">
                   <h5 class="card-title">Find Me Something New</h5>
+                  <img
+                    src={FMSN}
+                    alt="FMSN-Page"
+                    height="50%"
+                    width="70%"
+                  ></img>
                   <p class="card-text">
                     FMSN offers users an app to find new music with a low amount
                     of streams
@@ -30,6 +40,12 @@ export default function Portfolio() {
               <div class="card">
                 <div class="card-body">
                   <h5 class="card-title">Ftnss Jnks</h5>
+                  <img
+                    src={Ftnss}
+                    alt="Ftnss-Page"
+                    height="50%"
+                    width="70%"
+                  ></img>
                   <p class="card-text">
                     This tool will allow a user to plan a workout schedule as
                     well as find recipes to support their workouts
@@ -47,6 +63,7 @@ export default function Portfolio() {
               <div class="card">
                 <div class="card-body">
                   <h5 class="card-title">Password Generator</h5>
+                  <img src={PG} alt="FMSN-Page" height="50%" width="70%"></img>
                   <p class="card-text">
                     Using prompts, confirms and alerts a user can generate a
                     random password
@@ -64,6 +81,7 @@ export default function Portfolio() {
               <div class="card">
                 <div class="card-body">
                   <h5 class="card-title">Weather Dashboard</h5>
+                  <img src={WD} alt="WD-Page" height="50%" width="70%"></img>
                   <p class="card-text">
                     Allows users to search for a city and see both current and
                     upcoming weather
